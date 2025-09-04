2cbed1914e7382519b72ba2e22ee41c11fc09ed8
Steven Arizandieta <3817207210101@ingenieria.usac.edu.gt>
2025-09-03 18:39:01 -0600

    [ADD] Ventana para generar nuevos clientes
    - Se ha añadido la segunda interfaz gráfica, en la cual el usuario podrá añadir nuevos clientes a la cola.

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
+#Importamos solo tkinter
 import tkinter as tk
+#Importamos ttk para el combobox
+from tkinter import ttk
 
 # Ventana principal
 mainwin = tk.Tk()
@@ -34,6 +37,64 @@ class Table:
                 e.insert(tk.END, lst[i][j])
                 #Quiero que no toquen los valores ded los entrys que hacen la tabla:
                 e.config(state="readonly")
+
+
+#Generación de los pacientes
+#clase nuevo paciente:
+class paciente:
+    def __init__(self, nombre, edad, especialidad, tiempo_espera):
+        self.nombre = nombre
+        self.edad = edad
+        self.especialidad = especialidad
+        self.tiempo_espera = tiempo_espera
+
+#Como vamos a usar FIFO, tenemos que usar colas
+class cola:
+    def __init__(self):
+        self.cola = []
+
+    def encolar(self, paciente):
+        self.cola.append(paciente)
+
+    def desencolar(self):
+        self.cola.pop(0)    #El indice 0 en el pop es lo que diferencia una pila de una cola literal xd
+
+#Creación de ventana para guardar clientes
+def ventana_cliente():
+    newclientwin = tk.Toplevel()
+    newclientwin.geometry("600x300")
+
+    label = tk.Label(newclientwin, text="Generar nuevo cliente", font=('Arial', 14, 'bold'))
+    label.pack(pady=5) #Usamo pady paa los margenes
+    label1 = tk.Label(newclientwin, text="Llena los campos requeridos para continuar:", font=('Arial', 12))
+    label1.pack(pady=5)
+
+    label2 = tk.Label(newclientwin, text="Nombre de cliente: ")
+    label2.pack(pady=2)
+    txtBoxNombreCliente = tk.Entry(newclientwin, font=('Arial', 10))
+    txtBoxNombreCliente.pack(pady=2)
+    label2 = tk.Label(newclientwin, text="Edad: ")
+    label2.pack(pady=2)
+    txtBoxEdadCliente = tk.Entry(newclientwin, font=('Arial', 10))
+    txtBoxEdadCliente.pack(pady=2)
+    label2 = tk.Label(newclientwin, text="Nombre de cliente: ")
+    label2.pack(pady=2)
+
+    #Valores del combobox
+    opciones = ["Selecciona una opción", "Medicina general", "Pediatría", "Ginecología", "Dermatología"]
+    comboBoxEspecialidad = ttk.Combobox(newclientwin, values = opciones, state='readonly')
+    comboBoxEspecialidad.current(0)
+    comboBoxEspecialidad.pack(pady=2)
+
+    #Boton para agregar paciente a la cola:
+    boton1 = tk.Button(newclientwin, text="Añadir paciente a la cola!")
+    boton1.pack(pady=2)
+
+#Creamos boton para generar un nuevo paciente
+boton1 = tk.Button(mainwin, text = "Generar nuevo paciente", command = ventana_cliente)
+boton1.pack()
+
+
 
 # Crear la tabla dentro del frame
 t = Table(table_frame)
